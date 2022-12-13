fcb0fd34f7d7b18b6bca09296a15721c2a3a8925
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2022-12-13 15:02:22 +0100

    Update berkeley.sh

--- berkeley.sh
+++ berkeley.sh
@@ -7,7 +7,7 @@ apt-get install build-essential  libssl-dev libgmp-dev
 wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
 tar -xzvf db-4.8.30.NC.tar.gz
 cd db-4.8.30.NC/build_unix
-../dist/configure --enable-cxx
+../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
 make
 make install
 
