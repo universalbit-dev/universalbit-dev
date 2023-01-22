76ff01358800a96ec4f8d24181b2c1d78b90a6e9
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-01-22 10:20:21 +0100

    Delete litecoin.sh

--- blockchain/litecoin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-###Make your system ready::
-
-###Install Litecoin SuperNode 
-###
-wget https://raw.githubusercontent.com/litecoin-association/LitecoinNode/master/linux.sh -P /root/ ; bash /root/linux.sh 2>&1 | tee /root/install.log
