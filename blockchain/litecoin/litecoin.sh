60fbd4a64d43bd01cbf53bc5d4293efa46ef8257
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-01-22 10:19:56 +0100

    Create litecoin.sh

---
+++ blockchain/litecoin/litecoin.sh
@@ -0,0 +1,4 @@
+###Make your system ready:      
+###Install Litecoin SuperNode 
+
+wget https://raw.githubusercontent.com/litecoin-association/LitecoinNode/master/linux.sh -P /root/ ; bash /root/linux.sh 2>&1 | tee /root/install.log#!/bin/bash
