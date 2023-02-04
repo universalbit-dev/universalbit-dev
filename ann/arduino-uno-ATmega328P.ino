ff16187c277d03c8c85ce7078b57b1ca2e056c9d
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-05 00:25:27 +0100

    Create arduino-uno-ATmega328P.ino

---
+++ ann/arduino-uno-ATmega328P.ino
@@ -0,0 +1,324 @@
+/*
+UniversalBit ... once again
+about some copy and paste code
+Arduino NN
+Board:      Arduino Uno
+Processor:  ATmega328P
+*/
+
+/*******************************************************************
+ * ArduinoANN - An artificial neural network for the Arduino
+ * All basic settings can be controlled via the Network Configuration
+ * section.
+ * See robotics.hobbizine.com/arduinoann.html for details.
+ ******************************************************************/
+#include <SPI.h>
+#include <Ethernet.h>
+#include <math.h>
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = { 0xDE, 0xAB, 0xBF, 0xFE, 0xFA, 0xCD };
+IPAddress ip(192,168,1,143);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 8020 is default for HTTP):
+EthernetServer server(8020);
+void setup()
+{
+ // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  Ethernet.init(10);
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("NN WebServer");
+  Serial.println(Ethernet.localIP());
+   while (!Serial) {
+    ; 
+  }
+
+}
+/******************************************************************
+ * Network Configuration - customized per network
+ ******************************************************************/
+const int PatternCount = 10;
+const int InputNodes = 7;
+const int HiddenNodes = 8;
+const int OutputNodes = 4;
+const float LearningRate = 0.3;
+const float Momentum = 0.9;
+const float InitialWeightMax = 0.5;
+const float Success = 0.0004;
+const byte Input[PatternCount][InputNodes] = {
+  { 1, 1, 1, 1, 1, 1, 0 },  // 0
+  { 0, 1, 1, 0, 0, 0, 0 },  // 1
+  { 1, 1, 0, 1, 1, 0, 1 },  // 2
+  { 1, 1, 1, 1, 0, 0, 1 },  // 3
+  { 0, 1, 1, 0, 0, 1, 1 },  // 4
+  { 1, 0, 1, 1, 0, 1, 1 },  // 5
+  { 0, 0, 1, 1, 1, 1, 1 },  // 6
+  { 1, 1, 1, 0, 0, 0, 0 },  // 7
+  { 1, 1, 1, 1, 1, 1, 1 },  // 8
+  { 1, 1, 1, 0, 0, 1, 1 }   // 9
+};
+const byte Target[PatternCount][OutputNodes] = {
+  { 0, 0, 0, 0 },
+  { 0, 0, 0, 1 },
+  { 0, 0, 1, 0 },
+  { 0, 0, 1, 1 },
+  { 0, 1, 0, 0 },
+  { 0, 1, 0, 1 },
+  { 0, 1, 1, 0 },
+  { 0, 1, 1, 1 },
+  { 1, 0, 0, 0 },
+  { 1, 0, 0, 1 }
+};
+/******************************************************************
+ * End Network Configuration
+ ******************************************************************/
+int i, j, p, q, r;
+int ReportEvery1000;
+int RandomizedIndex[PatternCount];
+long  TrainingCycle;
+float Rando;
+float Error;
+float Accum;
+float Hidden[HiddenNodes];
+float Output[OutputNodes];
+float HiddenWeights[InputNodes+1][HiddenNodes];
+float OutputWeights[HiddenNodes+1][OutputNodes];
+float HiddenDelta[HiddenNodes];
+float OutputDelta[OutputNodes];
+float ChangeHiddenWeights[InputNodes+1][HiddenNodes];
+float ChangeOutputWeights[HiddenNodes+1][OutputNodes];
+
+void toTerminal()
+{
+  for( p = 0 ; p < PatternCount ; p++ ) {
+    Serial.println();
+    Serial.print ("  Training Pattern: ");
+    Serial.println (p);
+    Serial.print ("  Input ");
+    for( i = 0 ; i < InputNodes ; i++ ) {
+      Serial.print (Input[p][i], DEC);
+      Serial.print (" ");
+    }
+    Serial.print ("  Target ");
+    for( i = 0 ; i < OutputNodes ; i++ ) {
+      Serial.print (Target[p][i], DEC);
+      Serial.print (" ");
+    }
+/******************************************************************
+* Compute hidden layer activations
+******************************************************************/
+    for( i = 0 ; i < HiddenNodes ; i++ ) {
+      Accum = HiddenWeights[InputNodes][i] ;
+      for( j = 0 ; j < InputNodes ; j++ ) {
+        Accum += Input[p][j] * HiddenWeights[j][i] ;
+      }
+      Hidden[i] = 1.0/(1.0 + exp(-Accum)) ;
+    }
+
+/******************************************************************
+* Compute output layer activations and calculate errors
+******************************************************************/
+    for( i = 0 ; i < OutputNodes ; i++ ) {
+      Accum = OutputWeights[HiddenNodes][i] ;
+      for( j = 0 ; j < HiddenNodes ; j++ ) {
+        Accum += Hidden[j] * OutputWeights[j][i] ;
+      }
+      Output[i] = 1.0/(1.0 + exp(-Accum)) ;
+    }
+    Serial.print ("  Output ");
+    for( i = 0 ; i < OutputNodes ; i++ ) {
+      Serial.print (Output[i], 5);
+      Serial.print (" ");
+    }
+  }
+}
+
+void loop()
+{
+/******************************************************************
+* Initialize HiddenWeights and ChangeHiddenWeights
+******************************************************************/
+  for( i = 0 ; i < HiddenNodes ; i++ ) {
+    for( j = 0 ; j <= InputNodes ; j++ ) {
+      ChangeHiddenWeights[j][i] = 0.0 ;
+      Rando = float(random(100))/100;
+      HiddenWeights[j][i] = 2.0 * ( Rando - 0.5 ) * InitialWeightMax ;
+    }
+  }
+/******************************************************************
+* Initialize OutputWeights and ChangeOutputWeights
+******************************************************************/
+  for( i = 0 ; i < OutputNodes ; i ++ ) {
+    for( j = 0 ; j <= HiddenNodes ; j++ ) {
+      ChangeOutputWeights[j][i] = 0.0 ;
+      Rando = float(random(100))/100;
+      OutputWeights[j][i] = 2.0 * ( Rando - 0.5 ) * InitialWeightMax ;
+    }
+  }
+  Serial.println("Initial/Untrained Outputs: ");
+  toTerminal();
+/******************************************************************
+* Begin training
+******************************************************************/
+  for( TrainingCycle = 1 ; TrainingCycle < 2147483647 ; TrainingCycle++) {
+/******************************************************************
+* Randomize order of training patterns
+******************************************************************/
+    for( p = 0 ; p < PatternCount ; p++) {
+      q = random(PatternCount);
+      r = RandomizedIndex[p] ;
+      RandomizedIndex[p] = RandomizedIndex[q] ;
+      RandomizedIndex[q] = r ;
+    }
+    Error = 0.0 ;
+/******************************************************************
+* Cycle through each training pattern in the randomized order
+******************************************************************/
+    for( q = 0 ; q < PatternCount ; q++ ) {
+      p = RandomizedIndex[q];
+/******************************************************************
+* Compute hidden layer activations
+******************************************************************/
+      for( i = 0 ; i < HiddenNodes ; i++ ) {
+        Accum = HiddenWeights[InputNodes][i] ;
+        for( j = 0 ; j < InputNodes ; j++ ) {
+          Accum += Input[p][j] * HiddenWeights[j][i] ;
+        }
+        Hidden[i] = 1.0/(1.0 + exp(-Accum)) ;
+      }
+/******************************************************************
+* Compute output layer activations and calculate errors
+******************************************************************/
+      for( i = 0 ; i < OutputNodes ; i++ ) {
+        Accum = OutputWeights[HiddenNodes][i] ;
+        for( j = 0 ; j < HiddenNodes ; j++ ) {
+          Accum += Hidden[j] * OutputWeights[j][i] ;
+        }
+        Output[i] = 1.0/(1.0 + exp(-Accum)) ;
+        OutputDelta[i] = (Target[p][i] - Output[i]) * Output[i] * (1.0 - Output[i]) ;
+        Error += 0.5 * (Target[p][i] - Output[i]) * (Target[p][i] - Output[i]) ;
+      }
+/******************************************************************
+* Backpropagate errors to hidden layer
+******************************************************************/
+      for( i = 0 ; i < HiddenNodes ; i++ ) {
+        Accum = 0.0 ;
+        for( j = 0 ; j < OutputNodes ; j++ ) {
+          Accum += OutputWeights[i][j] * OutputDelta[j] ;
+        }
+        HiddenDelta[i] = Accum * Hidden[i] * (1.0 - Hidden[i]) ;
+      }
+/******************************************************************
+* Update Inner-->Hidden Weights
+******************************************************************/
+      for( i = 0 ; i < HiddenNodes ; i++ ) {
+        ChangeHiddenWeights[InputNodes][i] = LearningRate * HiddenDelta[i] + Momentum * ChangeHiddenWeights[InputNodes][i] ;
+        HiddenWeights[InputNodes][i] += ChangeHiddenWeights[InputNodes][i] ;
+        for( j = 0 ; j < InputNodes ; j++ ) {
+          ChangeHiddenWeights[j][i] = LearningRate * Input[p][j] * HiddenDelta[i] + Momentum * ChangeHiddenWeights[j][i];
+          HiddenWeights[j][i] += ChangeHiddenWeights[j][i] ;
+        }
+      }
+/******************************************************************
+* Update Hidden-->Output Weights
+******************************************************************/
+      for( i = 0 ; i < OutputNodes ; i ++ ) {
+        ChangeOutputWeights[HiddenNodes][i] = LearningRate * OutputDelta[i] + Momentum * ChangeOutputWeights[HiddenNodes][i] ;
+        OutputWeights[HiddenNodes][i] += ChangeOutputWeights[HiddenNodes][i] ;
+        for( j = 0 ; j < HiddenNodes ; j++ ) {
+          ChangeOutputWeights[j][i] = LearningRate * Hidden[j] * OutputDelta[i] + Momentum * ChangeOutputWeights[j][i] ;
+          OutputWeights[j][i] += ChangeOutputWeights[j][i] ;
+        }
+      }
+    }
+/******************************************************************
+* Every 1000 cycles send data to terminal for display
+******************************************************************/
+    ReportEvery1000 = ReportEvery1000 - 1;
+    if (ReportEvery1000 == 0)
+    {
+      Serial.println();
+      Serial.println();
+      Serial.print ("TrainingCycle: ");
+      Serial.print (TrainingCycle);
+      Serial.print ("  Error = ");
+      Serial.println (Error, 5);
+      toTerminal();
+      if (TrainingCycle==1)
+      {
+        ReportEvery1000 = 999;
+      }
+      else
+      {
+        ReportEvery1000 = 1000;
+      }
+    }
+/******************************************************************
+* If error rate is less than pre-determined threshold then end
+******************************************************************/
+    if( Error < Success ) break ;
+  }
+  Serial.println ();
+  Serial.println();
+  Serial.print ("TrainingCycle: ");
+  Serial.print (TrainingCycle);
+  Serial.print ("  Error = ");
+  Serial.println (Error, 5);
+  toTerminal();
+  Serial.println ();
+  Serial.println ();
+  Serial.println ("Training Set Solved! ");
+  Serial.println ("--------");
+  Serial.println ();
+  Serial.println ();
+  ReportEvery1000 = 1;
+  //
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == 'n' && currentLineIsBlank)
+        {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+    client.println("Refresh: 30");  // refresh the page automatically every 30 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // from here we can enter our own HTML code to create the web page
+          client.print("<head><title>Title</title></head><body></h1> - ");
+          client.print("<p><em>Page refreshes every 30 seconds<<em></p></body></html>.");
+          break;
+        }
+        if (c == 'n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != 'r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // delay to receive the data
+    delay(10);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
