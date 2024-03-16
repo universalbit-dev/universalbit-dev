bf86e70f3265b11f9347eae64f75c874298a01da
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-03-16 21:26:57 +0100

    Create cdn_united_states.js

---
+++ cdn/cdn_united_states.js
@@ -0,0 +1,10 @@
+/* Run Bash Script */
+var exec = require('node:child_process').exec
+exec('echo $(globalping dns cdn.jsdelivr.net from united states --limit 3)',
+    function (error, stdout, stderr) {
+        console.log('\x1b[33m%s\x1b[0m','Your Ip:' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+    });
