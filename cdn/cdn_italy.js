3a48347ab06ce38cf101a987603c37cd1ea572f6
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-03-19 02:02:54 +0100

    Update cdn_italy.js

--- cdn/cdn_italy.js
+++ cdn/cdn_italy.js
@@ -1,6 +1,6 @@
 /* Run Bash Script */
 var exec = require('node:child_process').exec
-exec('echo $(globalping ping cdn.jsdelivr.net from italy --limit 10)',
+exec('echo $(globalping dns cdn.jsdelivr.net from italy --limit 10)',
     function (error, stdout, stderr) {
         console.log('stderr: ' + stderr);
         if (error !== null) {
