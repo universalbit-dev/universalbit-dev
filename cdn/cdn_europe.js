1110a9f86a76381bd67e0b4694436181c5c88507
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-03-23 20:31:46 +0100

    Update cdn_europe.js

--- cdn/cdn_europe.js
+++ cdn/cdn_europe.js
@@ -2,6 +2,7 @@
 var exec = require('node:child_process').exec
 exec('echo $(globalping dns cdn.jsdelivr.net from europe --limit 3)',
     function (error, stdout, stderr) {
+        console.log('stdout:'+ stdout);
         console.log('stderr: ' + stderr);
         if (error !== null) {
              console.log('exec error: ' + error);
