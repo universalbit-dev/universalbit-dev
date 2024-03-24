db74f40b0938c0eaac97a1af512d08509278a523
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-03-24 11:29:44 +0100

    Update timeout.js

--- timeout/timeout.js
+++ timeout/timeout.js
@@ -13,18 +13,3 @@ function zzzsleep() {
 }
 zzzsleep();progressBar.addValue(1);
 console.log('  -- wait --');
-
-/*
-
-v2 with nodejs promise
-//https://www.linkedin.com/pulse/nodejs-16-settimeout-asyncawait-igor-gonchar
-import {setTimeout} from "timers/promises";
-console.log('Function sleep when Buy or Sell -- Activated --');
-async function newStyleDelay() {
-   await setTimeout(5000);
-   console.log('It will be printed 3-rd with delay');
-}
-newStyleDelay();
-console.log('It will be printed 2-nd');
-
-*/
