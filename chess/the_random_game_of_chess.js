38d489010503a97669b2730937d78ecef78cde44
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-08-28 19:06:28 +0200

    Add files via upload

---
+++ chess/the_random_game_of_chess.js
@@ -0,0 +1,21 @@
+const { Chess } = require('chess.js')
+
+thegameofchess=[];
+
+function fxchess() {
+  const chess = new Chess()
+  while (!chess.isGameOver()) {
+  const moves = chess.moves()
+  const move = moves[Math.floor(Math.random() * moves.length)]
+  chess.move(move)
+}
+return console.log(chess.pgn())
+}
+
+console.log("Random game of Chess");fxchess();
+
+module.exports = thegameofchess;
+
+
+
+
