9f8a9fa9b08379a80aee2302446cfea4db118496
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-10-27 17:01:34 +0100

    Update ecosystem.config.js

--- cdn/ecosystem.config.js
+++ cdn/ecosystem.config.js
@@ -24,29 +24,11 @@ module.exports = {
   instances : "1",
   exec_mode : ""
   },
-
   {
   name: 'italy',
   script    : 'cdn_italy.js',
   name      : '|Italy||',
   exp_backoff_restart_delay: 50,
-  instances : "1",
-  exec_mode : ""
-  },
-  {
-  name: 'italy_roma',
-  script    : 'cdn_italy_roma.js',
-  name      : '|Italy|Roma|',
-  exp_backoff_restart_delay: 500,
-  instances : "1",
-  exec_mode : ""
-  },
-
-  {
-  name: 'italy_palermo',
-  script    : 'cdn_italy_palermo.js',
-  name      : '|Italy|Palermo|',
-  exp_backoff_restart_delay: 500,
   instances : "1",
   exec_mode : ""
   },
@@ -59,15 +41,6 @@ module.exports = {
   exec_mode : ""
   },
   {
-  name: 'france_paris',
-  script    : 'cdn_france_paris.js',
-  name      : '|France|Paris|',
-  exp_backoff_restart_delay: 5000,
-  instances : "1",
-  exec_mode : ""
-  },
-
-  {
   name: 'germany',
   script    : 'cdn_germany.js',
   name      : '|Germany||',
@@ -75,15 +48,6 @@ module.exports = {
   instances : "1",
   exec_mode : ""
   },
-  {
-  name: 'germany_berlin',
-  script    : 'cdn_germany_berlin.js',
-  name      : '|Germany|Berlin|',
-  exp_backoff_restart_delay: 5000,
-  instances : "1",
-  exec_mode : ""
-  },
-
   {
   name: 'spain',
   script    : 'cdn_spain.js',
@@ -93,22 +57,12 @@ module.exports = {
   exec_mode : ""
   },
   {
-  name: 'spain_madrid',
-  script    : 'cdn_spain_madrid.js',
-  name      : '|Spain|Madrid|',
-  exp_backoff_restart_delay: 500,
-  instances : "1",
-  exec_mode : ""
-  },
-  {
   name: 'restart',
   script    : 'restart.js',
   name      : '|Restart||',
   exp_backoff_restart_delay: 50000,
-  instances : "1",
   exec_mode : ""
   }
-
 ]
 
 }
