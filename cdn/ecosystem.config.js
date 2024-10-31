eebbb7366444e486a71ea415b620c398f7b57d50
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-10-31 19:36:04 +0100

    Update ecosystem.config.js

--- cdn/ecosystem.config.js
+++ cdn/ecosystem.config.js
@@ -4,169 +4,85 @@ module.exports = {
   name: 'world',
   script    : 'cdn.js',
   name      : '|WORLD CDN ||',
-  exec_mode : "cluster"
+  exec_mode : "cluster",
+  instances : "max"
   },
   {
   name: 'europe',
   script    : 'cdn_europe.js',
   name      : '|EUROPE CDN ||',
-  exec_mode : "cluster"
+  exec_mode : "cluster",
+  instances : "max"
   },
   {
   name: 'united states',
   script    : 'cdn_united_states.js',
   name      : '|USA CDN ||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'italy',
-  script    : 'cdn_italy.js',
-  name      : '|Italy CDN ||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'france',
-  script    : 'cdn_france.js',
-  name      : '|France CDN ||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'germany',
-  script    : 'cdn_germany.js',
-  name      : '|Germany CDN||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'spain',
-  script    : 'cdn_spain.js',
-  name      : '|Spain CDN ||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'world',
-  script    : 'www.universalbit.it/cdn.js',
-  name      : '|WORLD WWW UNBT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'europe',
-  script    : 'www.universalbit.it/cdn_europe.js',
-  name      : '|EUROPE WWW UNBT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'united states',
-  script    : 'www.universalbit.it/cdn_united_states.js',
-  name      : '|USA WWW UNBT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'italy',
-  script    : 'www.universalbit.it/cdn_italy.js',
-  name      : '|Italy WWW UNBT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'france',
-  script    : 'www.universalbit.it/cdn_france.js',
-  name      : '|France WWW UNBT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'germany',
-  script    : 'www.universalbit.it/cdn_germany.js',
-  name      : '|Germany WWW UNBT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'spain',
-  script    : 'www.universalbit.it/cdn_spain.js',
-  name      : '|Spain WWW UNBT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'world',
-  script    : 'universalbit.it/cdn.js',
-  name      : '|WORLD UNIVERSALBIT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'europe',
-  script    : 'universalbit.it/cdn_europe.js',
-  name      : '|EUROPE UNIVERSALBIT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'united states',
-  script    : 'universalbit.it/cdn_united_states.js',
-  name      : '|USA UNIVERSALBIT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'italy',
-  script    : 'universalbit.it/cdn_italy.js',
-  name      : '|Italy UNIVERSALBIT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'france',
-  script    : 'universalbit.it/cdn_france.js',
-  name      : '|France UNIVERSALBIT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'germany',
-  script    : 'universalbit.it/cdn_germany.js',
-  name      : '|Germany UNIVERSALBIT||',
-  exec_mode : "cluster"
-  },
-  {
-  name: 'spain',
-  script    : 'universalbit.it/cdn_spain.js',
-  name      : '|Spain UNIVERSALBIT||',
-  exec_mode : "cluster"
+  exec_mode : "cluster",
+  instances : "max"
   },
   {
   name: 'world',
   script    : '93018.ddns.net/cdn.js',
   name      : '|WORLD 93018||',
-  exec_mode : "cluster"
+  exec_mode : "cluster",
+  instances : "max"
   },
   {
   name: 'europe',
   script    : '93018.ddns.net/cdn_europe.js',
   name      : '|EUROPE 93018||',
-  exec_mode : "cluster"
+  exec_mode : "cluster",
+  instances : "max"
   },
   {
   name: 'united states',
   script    : '93018.ddns.net/cdn_united_states.js',
   name      : '|USA 93018||',
-  exec_mode : "cluster"
+  exec_mode : "cluster",
+  instances : "max"
   },
   {
-  name: 'italy',
-  script    : '93018.ddns.net/cdn_italy.js',
-  name      : '|Italy 93018||',
-  exec_mode : "cluster"
+  name: 'world',
+  script    : 'www.universalbit.it/cdn.js',
+  name      : '|WORLD WWW UNBT||',
+  exec_mode : "cluster",
+  instances : "max"
   },
   {
-  name: 'france',
-  script    : '93018.ddns.net/cdn_france.js',
-  name      : '|France 93018||',
-  exec_mode : "cluster"
+  name: 'europe',
+  script    : 'www.universalbit.it/cdn_europe.js',
+  name      : '|EUROPE WWW UNBT||',
+  exec_mode : "cluster",
+  instances : "max"
   },
   {
-  name: 'germany',
-  script    : '93018.ddns.net/cdn_germany.js',
-  name      : '|Germany 93018||',
-  exec_mode : "cluster"
+  name: 'united states',
+  script    : 'www.universalbit.it/cdn_united_states.js',
+  name      : '|USA WWW UNBT||',
+  exec_mode : "cluster",
+  instances : "max"
   },
   {
-  name: 'spain',
-  script    : '93018.ddns.net/cdn_spain.js',
-  name      : '|Spain 93018||',
-  exec_mode : "cluster"
+  name: 'world',
+  script    : 'universalbit.it/cdn.js',
+  name      : '|WORLD UNIVERSALBIT||',
+  exec_mode : "cluster",
+  instances : "max"
+  },
+  {
+  name: 'europe',
+  script    : 'universalbit.it/cdn_europe.js',
+  name      : '|EUROPE UNIVERSALBIT||',
+  exec_mode : "cluster",
+  instances : "max"
+  },
+  {
+  name: 'united states',
+  script    : 'universalbit.it/cdn_united_states.js',
+  name      : '|USA UNIVERSALBIT||',
+  exec_mode : "cluster",
+  instances : "max"
   },
   {
   name: 'restart',
@@ -174,7 +90,13 @@ module.exports = {
   name      : '|Restart||',
   exp_backoff_restart_delay: 500000,
   exec_mode : "fork"
+  },
+  {
+  name: 'autoclean',
+  script    : 'autoclean.js',
+  name      : '|AutoClean||',
+  exp_backoff_restart_delay: 500000,
+  exec_mode : "fork"
   }
 ]
-
 }
