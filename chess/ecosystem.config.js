d30b72641c0b51a8f80fc55affa001a3047fcbef
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-12-09 22:19:33 +0100

    Add files via upload

--- chess/ecosystem.config.js
+++ chess/ecosystem.config.js
@@ -6,6 +6,13 @@ module.exports = {
   name      : '|CHESS||',
   exec_mode: 'fork',
   exp_backoff_restart_delay: 10000
+  },
+  {
+  name: 'chessout',
+  script    : 'the_random_game_of_chess_out.js',
+  name      : '|CHESSOUT||',
+  exec_mode: 'fork',
+  exp_backoff_restart_delay: 10000
   }
 ]
 
