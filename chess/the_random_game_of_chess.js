115b1e4f28e9eb6506343b10ee7f123c2b929633
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-12-14 22:48:51 +0100

    Update the_random_game_of_chess.js

--- chess/the_random_game_of_chess.js
+++ chess/the_random_game_of_chess.js
@@ -1,21 +1,20 @@
 const { addon: ov } = require('openvino-node');
-const { Chess } = require('chess.js')
-
+const { Chess } = require('chess.js');
+var fs = require('fs/promises');
 thegameofchess=[];
 
-function fxchess() {
+async function fxchess() {
   const chess = new Chess()
   while (!chess.isGameOver()) {
   const moves = chess.moves()
   const move = moves[Math.floor(Math.random() * moves.length)]
   chess.move(move)
+}  
+return await fs.appendFile("chessout.json",JSON.stringify(chess.pgn()));
 }
-return console.log(chess.pgn())
-}
-
-console.log("Random game of Chess");fxchess();
-
+thegameofchess=[fxchess()];
 module.exports = thegameofchess;
+
 
 
 
