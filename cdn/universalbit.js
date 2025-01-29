629f86c3e9d1cf6cbb06d468d0421018e6d87562
UniversalBit <lazzaragiuliano@gmail.com>
2025-01-29 15:20:59 +0100

    Update universalbit.js

--- cdn/universalbit.js
+++ cdn/universalbit.js
@@ -1,27 +1,7 @@
 /* GlobalPing UniversalBit Project */
 var universalbit = {};
 var unbtit = require('node:child_process').exec
-unbtit('echo $(globalping dns universalbitcdn.it from world --limit 5  )',
-    function (error, stdout, stderr) {
-        console.log('stdout:'+ stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
-});
-
-var unbtrepo = require('node:child_process').exec
-unbtrepo('echo $(globalping dns universalbit.it from world --limit 5  )',
-    function (error, stdout, stderr) {
-        console.log('stdout:'+ stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
-});
-
-var unbtsite = require('node:child_process').exec
-unbtsite('echo $(globalping dns www.universalbit.it from world --limit 5  )',
+unbtit('echo $(ping cdn.jsdelivr.net -i 10  )',
     function (error, stdout, stderr) {
         console.log('stdout:'+ stdout);
         console.log('stderr: ' + stderr);
