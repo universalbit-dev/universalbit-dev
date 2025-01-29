60ea6a5df8b42ed48a6aee1bf102aaeaeef18dd5
UniversalBit <lazzaragiuliano@gmail.com>
2025-01-29 15:27:16 +0100

    Update ecosystem.config.js

--- cdn/ecosystem.config.js
+++ cdn/ecosystem.config.js
@@ -3,86 +3,9 @@ module.exports = {
   {
   name: 'world',
   script    : 'cdn.js',
-  name      : '|WORLD CDN ||',
+  name      : '|Globalping CDN ||',
   exec_mode : "cluster",
   instances : "1"
-  },
-  {
-  name: 'planet debian',
-  script    : 'cdn_planet_debian.js',
-  name      : '|Planet Debian CDN ||',
-  exec_mode : "fork"
-  },
-  {
-  name: 'europe',
-  script    : 'cdn_europe.js',
-  name      : '|EUROPE CDN ||',
-  exec_mode : "cluster",
-  instances : "1"
-  },
-  {
-  name: 'united states',
-  script    : 'cdn_united_states.js',
-  name      : '|USA CDN ||',
-  exec_mode : "cluster",
-  instances : "1"
-  },
-  {
-  name: 'world',
-  script    : '93018.ddns.net/cdn.js',
-  name      : '|WORLD 93018||',
-  exec_mode : "cluster",
-  instances : "1"
-  },
-  {
-  name: 'europe',
-  script    : '93018.ddns.net/cdn_europe.js',
-  name      : '|EUROPE 93018||',
-  exec_mode : "cluster",
-  instances : "1"
-  },
-  {
-  name: 'united states',
-  script    : '93018.ddns.net/cdn_united_states.js',
-  name      : '|USA 93018||',
-  exec_mode : "cluster",
-  instances : "1"
-  },
-  {
-  name: 'world',
-  script    : 'www.universalbit.it/cdn.js',
-  name      : '|WORLD WWW UNBT||',
-  exec_mode : "fork"
-  },
-  {
-  name: 'europe',
-  script    : 'www.universalbit.it/cdn_europe.js',
-  name      : '|EUROPE WWW UNBT||',
-  exec_mode : "fork"
-  },
-  {
-  name: 'united states',
-  script    : 'www.universalbit.it/cdn_united_states.js',
-  name      : '|USA WWW UNBT||',
-  exec_mode : "fork"
-  },
-  {
-  name: 'world',
-  script    : 'universalbit.it/cdn.js',
-  name      : '|WORLD UNIVERSALBIT||',
-  exec_mode : "fork"
-  },
-  {
-  name: 'europe',
-  script    : 'universalbit.it/cdn_europe.js',
-  name      : '|EUROPE UNIVERSALBIT||',
-  exec_mode : "fork"
-  },
-  {
-  name: 'united states',
-  script    : 'universalbit.it/cdn_united_states.js',
-  name      : '|USA UNIVERSALBIT||',
-  exec_mode : "fork"
   },
   {
   name: 'unbt',
