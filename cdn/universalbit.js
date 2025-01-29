6779b41836ae45bc6b3f9efd64df26b85fc188a1
UniversalBit <lazzaragiuliano@gmail.com>
2025-01-29 15:23:16 +0100

    Update universalbit.js

--- cdn/universalbit.js
+++ cdn/universalbit.js
@@ -1,13 +1,13 @@
 /* GlobalPing UniversalBit Project */
 var universalbit = {};
-var unbtit = require('node:child_process').exec
-unbtit('echo $(ping cdn.jsdelivr.net -i 10  )',
-    function (error, stdout, stderr) {
-        console.log('stdout:'+ stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
+var unbtit = require('node:child_process').exec;
+
+unbtit('echo $(ping cdn.jsdelivr.net -i 10)', function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
 });
 
 module.exports = universalbit;
